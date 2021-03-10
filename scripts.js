9ab0564f1e01049d407c1c78162e9d77e47a4873
ledluxe <79418525+ledluxe@users.noreply.github.com>
2021-03-10 21:03:46 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
   }
   async function sendMessage({ name, phone }) {
     const BOT_API_KEY = "1606647901:AAFykXDStBiPeeMBR6LmHA9HGvCmQOK38Dc";
-    const CHANNEL_ID = "638926333";
+    const CHANNEL_ID = "1001452408584";
     const text = `Name: ${name} \t
 		\n TEL: ${phone} `;
 
